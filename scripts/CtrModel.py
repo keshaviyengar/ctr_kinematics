2b32d68aed38308ef114bdf491bb249ed394f72a
Keshav Iyengar <keshav.iyengar94@gmail.com>
2020-01-09 21:35:58 -0500

    updates for a more accurate model.

--- scripts/CtrModel.py
+++ scripts/CtrModel.py
@@ -44,25 +44,25 @@ class SegmentRobot(object):
         u_y = np.zeros((3, segment_length.size))
 
         for i in range(n):
+            # +1 because, one point is at 0
             # find where the tube begins
-            aa = np.where(index == i + 1)
-            a = aa[0]
+            a = np.where(index == i + 1)[0]
             # find where tube curve starts
-            bb = np.where(index == i + 4)
-            b = bb[0]
+            b = np.where(index == i + 4)[0]
             # Find where tube ends
-            cc = np.where(index == i + 7)
-            c = cc[0]
+            c = np.where(index == i + 7)[0]
             if segment_length[a] == 0:
                 a += 1
             if segment_length[b] == 0:
                 b += 2
-            if c.item() <= segment_length.size - 1:
-                if segment_length[c] == 0:
-                    c += 1
+            if (c.item() <= segment_length.size - 1) and (segment_length[c] == 0):
+                c += 1
 
+            # For the segment a to c, set the stiffness for tube i
             e[i, np.arange(a, c)] = stiffness[i]
+            # For segment b to c set curvature for tube i in x direction
             u_x[i, np.arange(b, c)] = curve_x[i]
+            # For segment b to c set curvature for tube i in y direction
             u_y[i, np.arange(b, c)] = curve_y[i]
 
         # Removing zero lengths
@@ -71,11 +71,13 @@ class SegmentRobot(object):
         uu_x = u_x[:, np.nonzero(segment_length)]
         uu_y = u_y[:, np.nonzero(segment_length)]
 
+        # Cumulative sum to determine length from origin
         length_sum = np.cumsum(length)
-        # s is the segmented abscissa of the tube after template
+        # s is the segmented abscissa (y-coordinate) of the tube after template (s=0)
         self.S = length_sum[length_sum + min(base) > 0] + min(base)
         # Truncating matrices, removing elements that correspond to the tube before template
-        e_t = ee[length_sum + min(base) > 0 * ee].reshape(3, len(self.S))
+        e_t = ee[length_sum + min(base) > np.zeros_like(ee)].reshape(3, len(self.S))
+        # Each (i,j) element of above matrices correspond to jth segment of ith tube where first tube is most inner
         self.EI = (e_t.T * np.array([t1.I, t2.I, t3.I])).T
         self.U_x = uu_x[length_sum + min(base) > 0 * ee].reshape(3, len(self.S))
         self.U_y = uu_y[length_sum + min(base) > 0 * ee].reshape(3, len(self.S))
@@ -83,8 +85,7 @@ class SegmentRobot(object):
 
 
 class CTRModel(object):
-    def __init__(self, tube_1, tube_2, tube_3, base):
-        self.segments = SegmentRobot(tube_1, tube_2, tube_3, base)
+    def __init__(self, tube_1, tube_2, tube_3):
         self.tube_1 = tube_1
         self.tube_2 = tube_2
         self.tube_3 = tube_3
@@ -92,25 +93,44 @@ class CTRModel(object):
         rospy.init_node("exact_ctr_kinematics")
         self.br = tf.TransformBroadcaster()
 
-    def run(self, q, q0, uz_0, alpha_0, r_0, R_0):
-        # position of tubes' base from template (i.e., s=0)
-        beta = q[0:3] + q0[0:3]
-        self.segments = SegmentRobot(self.tube_1, self.tube_2, self.tube_3, beta)
-        r, u_z_end, tip_pos = self.fk(uz_0, alpha_0, r_0, R_0)
-        self.publish_transforms(r)
+    # q[0:2] are extension values and q[3:5] rotation values from the base
+    # q0 are starting joint values
+    # uz_0 is column vector of initial torsion about z axis
+    # r_0 is base position
+    # R_0 is initial base orientation
+    # def fk(self, q, q0, uz_0, alpha_0, r_0, R_0):
+    def fk(self, q, **kwargs):
+        if 'q_0' not in kwargs:
+            kwargs['q_0'] = np.array([0, 0, 0, 0, 0, 0])
+        if 'r_0' not in kwargs:
+            kwargs['r_0'] = np.array([0, 0, 0]).reshape(3, 1)
+        if 'uz_0' not in kwargs:
+            # initial twist
+            kwargs['uz_0'] = np.array([0, 0, 0])
 
-    def fk(self, uz_0, alpha_0, r_0, R_0):
+        uz_0 = kwargs['uz_0']
+        r_0 = kwargs['r_0']
+        alpha_1_0 = q[3] + kwargs['q_0'][3]
+        R_0 = np.array(
+            [[np.cos(alpha_1_0), -np.sin(alpha_1_0), 0], [np.sin(alpha_1_0), np.cos(alpha_1_0), 0], [0, 0, 1]]) \
+            .reshape(9, 1)
+        alpha_0 = q[3:].reshape(3, 1) + kwargs['q_0'][3:].reshape(3, 1)
+
+        # position of tubes' base from template (i.e., s=0)
+        beta = q[0:3] + kwargs['q_0'][0:3]
+        segments = SegmentRobot(self.tube_1, self.tube_2, self.tube_3, beta)
+
         Length = np.empty(0)
         r = np.empty((0, 3))
         u_z = np.empty((0, 3))
         alpha = np.empty((0, 3))
-        span = np.append([0], self.segments.S)
-        for seg in range(0, len(self.segments.S)):
+        span = np.append([0], segments.S)
+        for seg in range(0, len(segments.S)):
             # Initial conditions, 3 initial twist + 3 initial angle + 3 initial position + 9 initial rotation matrix
             y_0 = np.vstack((uz_0.reshape(3, 1), alpha_0, r_0, R_0)).ravel()
             s_span = np.linspace(span[seg], span[seg + 1] - 1e-6, num=30)
-            s = odeint(self.ode_eq, y_0, s_span, args=(self.segments.U_x[:, seg], self.segments.U_y[:, seg],
-                                                       self.segments.EI[:, seg], self.segments.GJ))
+            s = odeint(self.ode_eq, y_0, s_span, args=(segments.U_x[:, seg], segments.U_y[:, seg],
+                                                       segments.EI[:, seg], segments.GJ))
             Length = np.append(Length, s_span)
             u_z = np.vstack((u_z, s[:, (0, 1, 2)]))
             alpha = np.vstack((alpha, s[:, (3, 4, 5)]))
@@ -133,9 +153,14 @@ class CTRModel(object):
         return r, u_z_end, tip_pos
 
     def ode_eq(self, y, s, ux_0, uy_0, ei, gj):
+        # first num_tubes elements represent curvatures along z
+        # second num_tubes elements represent twist angles, alpha_i
+        # last 12 elements are r (positions), R (orientations)
         dydt = np.empty([18, 1])
         ux = np.empty([3, 1])
         uy = np.empty([3, 1])
+
+        # calculate, for each tube, tube curvature in x and y direction
         for i in range(3):
             ux[i] = (1 / (ei[0] + ei[1] + ei[2])) * \
                 (ei[0] * ux_0[0] * np.cos(y[3 + i] - y[3 + 0]) + ei[0] * uy_0[0] * np.sin(y[3 + i] - y[3 + 0]) +
@@ -145,6 +170,7 @@ class CTRModel(object):
                     (-ei[0] * ux_0[0] * np.sin(y[3 + i] - y[3 + 0]) + ei[0] * uy_0[0] * np.cos(y[3 + i] - y[3 + 0]) +
                      -ei[1] * ux_0[1] * np.sin(y[3 + i] - y[3 + 1]) + ei[1] * uy_0[1] * np.cos(y[3 + i] - y[3 + 1]) +
                      -ei[2] * ux_0[2] * np.sin(y[3 + i] - y[3 + 2]) + ei[2] * uy_0[2] * np.cos(y[3 + i] - y[3 + 2]))
+        # ode for twist
         for j in range(3):
             if ei[j] == 0:
                 # ui_z
@@ -158,6 +184,8 @@ class CTRModel(object):
                 dydt[3 + j] = y[j]
 
         e3 = np.array([0, 0, 1]).reshape(3, 1)
+        # y[0:2] are position of point materials
+        # y[3:end] rotation matrix elements
         uz = y[0:3]
         R = np.array(y[9:]).reshape(3, 3)
         u_hat = np.array([(0, - uz[0], uy[0]), (uz[0], 0, -ux[0]), (-uy[0], ux[0], 0)])
@@ -196,25 +224,11 @@ if __name__ == '__main__':
     tube3 = TubeParameters(length=174e-3, length_curved=134e-3, inner_diameter=2e-3, outer_diameter=2 * 1.1e-3,
                            stiffness=4.7163091968e+10, torsional_stiffness=2.9788923392e+10, x_curvature=3.5,
                            y_curvature=0)
-    # Joint variables
-    q = np.array([0.01, 0.015, 0.019, np.pi, np.pi * 5 / 2, np.pi / 2])
-    # Initial position of joints
-    q_0 = np.array([-0.2858, -0.2025, -0.0945, 0, 0, 0])
-    # position of tubes' base from template (i.e., s=0)
-    beta = q[0:3] + q_0[0:3]
-
-    ctr_model = CTRModel(tube1, tube2, tube3, beta)
-
-    r_0_ = np.array([0, 0, 0]).reshape(3, 1)
-    alpha_1_0 = q[3] + q_0[3]
-    R_0_ = np.array([[np.cos(alpha_1_0), -np.sin(alpha_1_0), 0], [np.sin(alpha_1_0), np.cos(alpha_1_0), 0], [0, 0, 1]])\
-        .reshape(9, 1)
-    alpha_0_ = q[3:].reshape(3, 1) + q_0[3:].reshape(3, 1)
-
-    # initial twist
-    uz_0_ = np.array([0, 0, 0])
-
+    # Joint variables(Beta)
+    q = np.array([-tube1.L, 0, 0, 0, 0, 0])
+    ctr_model = CTRModel(tube1, tube2, tube3)
     while not rospy.is_shutdown():
-        ctr_model.run(q, q_0, uz_0_, alpha_0_, r_0_, R_0_)
+        r, u_z_end, tip_pos = ctr_model.fk(q)
+        ctr_model.publish_transforms(r)
         rospy.sleep(0.5)
 
--- scripts/ctr_env.py
+++ scripts/ctr_env.py
@@ -1,26 +1,39 @@
 import gym
-import rospy
 import numpy as np
-from ctr_kinematics.srv import ComputeForwardKinematics
-from sensor_msgs.msg import JointState
+
+from CtrModel import TubeParameters
+from CtrModel import CTRModel
 
 
 # ROS environment to be used with RVim CTR kinematics library for variable curvature / stability modelling
 class CtrEnv(gym.GoalEnv):
     def __init__(self):
-        # Initialize rospy node and service
-        rospy.init_node("ctr_gym_environment")
-        self.fk_service = rospy.ServiceProxy('fk_service', ComputeForwardKinematics)
-        # Initialize number of tubes(maybe from params server)
-        self.num_tubes = 2
+        # Set tube parameters
+        self.n = 3
+        tube1 = TubeParameters(length=431e-3, length_curved=103e-3, inner_diameter=2 * 0.35e-3,
+                               outer_diameter=2 * 0.55e-3,
+                               stiffness=6.4359738368e+10, torsional_stiffness=2.5091302912e+10, x_curvature=21.3,
+                               y_curvature=0)
+        tube2 = TubeParameters(length=332e-3, length_curved=113e-3, inner_diameter=2 * 0.7e-3,
+                               outer_diameter=2 * 0.9e-3,
+                               stiffness=5.2548578304e+10, torsional_stiffness=2.1467424256e+10, x_curvature=13.108,
+                               y_curvature=0)
+        tube3 = TubeParameters(length=174e-3, length_curved=134e-3, inner_diameter=2e-3, outer_diameter=2 * 1.1e-3,
+                               stiffness=4.7163091968e+10, torsional_stiffness=2.9788923392e+10, x_curvature=3.5,
+                               y_curvature=0)
+
+        # Initialize CTRModel
+        self.ctr_model = CTRModel(tube1, tube2, tube3)
+
         # Seperately store joint values normally and also have cylindrical output for observation
-        self.joint_state = np.empty([2, self.num_tubes])
-        self.trig_joint_values = np.empty([3, self.num_tubes])
+        self.joint_state = np.empty([2, self.n])
+        self.trig_joint_values = np.empty([3, self.n])
         # Initialize action space limits
 
         self.goal_tolerance = 0.001
 
     def reset(self):
+        # Resample goal
         pass
 
     def seed(self, seed=None):
@@ -28,12 +41,10 @@ class CtrEnv(gym.GoalEnv):
             np.random.seed(seed)
 
     def step(self, action):
-        # Add action to joint values, convert to cylindrical representation
-        # Call ROS service to compute FK
-        joint_state_srv = JointState()
-        joint_state_srv.position = self.joint_state
-        response = self.fk_service(joint_state_srv)
-        ee_pos = response.ee_pos
+        # Add action to current joint state
+        # Compute FK
+        # Convert to cylindrical representation
+        # Return observation, reward, done, info
         pass
 
     def compute_reward(self, achieved_goal, desired_goal, info):
